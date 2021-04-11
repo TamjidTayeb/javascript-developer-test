ac5349398ab87908285bd76f2bbe6549ab65bac6
Tamjid Tayeb <38994890+TamjidTayeb@users.noreply.github.com>
2021-04-11 15:24:17 +1000

    all tests passing

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,31 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+// Constants
+const OK_RES = 200
+const SUCC_KEY = 'Arnie Quote'
+const FAIL_KEY = 'FAILURE'
+
+const getArnieQuotes = (urls) => {
+  // Map over the urls and return an array of the initiated promises
+  const promises = urls.map( async url => {
+    try {
+      // Initiate get request
+      const response = await httpGet(url)
+      
+      // Extract response status
+      const { status } = response
+      // Parse response body and extract message
+      const { message } = JSON.parse(response.body)
+
+      // Return object with key based on response status
+      const key = status === OK_RES ? SUCC_KEY : FAIL_KEY
+      return { [key]: message }
+    } catch(err) {
+      return err
+    }
+  })
+  // Return the array of pending promises which will resolve to objects containing arnie quotes
+  return Promise.all(promises);
 };
 
 module.exports = {
